1244a0272021dc4580d63a9e7ea80703a51de0e9
alao1908 <gm_douzi@esi.dz>
2022-01-14 17:25:33 +0100

    apdated stage model

--- stage/models.py
+++ stage/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 
 class Etat(models.TextChoices):
-    SUJET_EN_ATTENTE_VALIDATION = 'sujet en Attente validation',
+    SUJET_BROUILLON = 'brouillon de sujet',  # sujet non soumis
+    SUJET_EN_ATTENTE_VALIDATION = 'sujet en Attente validation',  # sujet soumis
     SUJET_VALIDE = 'sujet validé',
     SUJET_SOUS_REVERVE = 'sujet sous reserve',
     SUJET_REJETE = 'sujet rejeté',
@@ -28,17 +29,18 @@ class Modalites_versements(models.TextChoices):
 
 class Stage(models.Model):
     etat = models.CharField(
-        max_length=45, choices=Etat.choices, default=Etat.SUJET_EN_ATTENTE_VALIDATION)
+        max_length=45, choices=Etat.choices, default=Etat.SUJET_BROUILLON)
     intitule = models.TextField()  # sujet stage
     description = models.TextField()
     documents_sup = models.FileField(null=True)
     date_debut = models.DateField()
-    date_fin = models.DateField()
+    date_fin = models.DateField(null=True)
     # Jours et horaires de travail
     date_debut_interruption = models.DateField(null=True)
     date_fin_interruption = models.DateField(null=True)
-    nombre_heure_total = models.IntegerField()
+    nombre_heure = models.IntegerField()  # nombre_heure par semaine
     remunere = models.BooleanField()
+    confidentiel = models.BooleanField(default=False)
     remuneration = models.IntegerField(null=True)
     modalites_versements = models.CharField(
         max_length=15, choices=Modalites_versements.choices, default=Modalites_versements.NON_CONNU)
